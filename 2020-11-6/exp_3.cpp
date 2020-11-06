d5444d4294077151a0cd862386fea8dfac8b4c6b
tnb918 <2891199724@qq.com>
2020-11-06 13:18:06 +0800

    更改

--- 2020-11-6/exp_3.cpp
+++ 2020-11-6/exp_3.cpp
@@ -10,14 +10,14 @@ int main()
 
 	//变换前的三点坐标
 	const Point2f src_pt[] = {
-		Point2f(200,200),
-		Point2f(250,200),
-		Point2f(200,100) };
+								Point2f(200,200),
+								Point2f(250,200),
+								Point2f(200,100) };
 	//变换后的三点坐标
 	const Point2f dst_pt[] = {
-		Point2f(300,100),
-		Point2f(300,50),
-		Point2f(200,100) };
+								Point2f(300,100),
+								Point2f(300,50),
+								Point2f(200,100) };
 	//计算仿射矩阵
 	const Mat affine_matrix = getAffineTransform(src_pt, dst_pt);
 	warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
