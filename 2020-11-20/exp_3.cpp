2b18c983fa3e9fc4ad36a1751ce1a749f6bd6463
tnb918 <2891199724@qq.com>
2020-11-20 14:37:43 +0800

    更改

--- 2020-11-20/exp_3.cpp
+++ 2020-11-20/exp_3.cpp
@@ -30,7 +30,7 @@ int main()
 	inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
 	imshow("detectMat", detectMat);
 
-	//构建结构元素，进行闭运算,填充缝隙，得到整体轮廓
+	//构建结构元素，进行闭运算，填充缝隙，得到整体轮廓
 	Mat stru = getStructuringElement(MORPH_RECT, Size(23, 23), Point(-1, -1));
 	morphologyEx(detectMat, detectMat_close, MORPH_CLOSE, stru, Point(-1, -1), 1);
 	imshow("detectMat_close", detectMat_close);
